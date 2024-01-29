206bddf1997fdefbb7620dcb5420b9e090ea32ee
Jahnph <Jahnph@students.uni-marburg.de>
2024-01-29 12:42:07 +0100

    updated impl to correct output format

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -17,19 +17,14 @@ object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
     import spark.implicits._
-    val names = new Array[Array[String]](inputs.size)
-    val fileIndices = new Array[Broadcast[Int]](inputs.size)
-    var pipeline = spark.emptyDataset[(String, Set[(Int, Int)])].rdd
-    for (input <- inputs.indices) {
-      val bcInput = spark.sparkContext.broadcast(input)
-      fileIndices(input) = bcInput
-      val dataFromFile = readData(inputs(input), spark)
-      names(input) = dataFromFile.schema.fieldNames
 
+    var pipeline = spark.emptyDataset[(String, Set[String])].rdd
+    for (input <- inputs.indices) {
+      val dataFromFile = readData(inputs(input), spark)
       val pipe = dataFromFile.flatMap(row => {
-        val list = new Array[(String, Set[(Int, Int)])](row.size)
-        for (value <- 0 until row.size) {
-          list(value) = (row.getString(value), Set((bcInput.value, value)))
+        val list = new Array[(String, Set[String])](row.size)
+        for (value <- row.schema.indices) {
+          list(value) = (row.getString(value), Set(row.schema.fieldNames(value)))
         }
         list
       })
@@ -40,7 +35,7 @@ object Sindy {
     val result = pipeline
       .reduceByKey((v1, v2) => v1.union(v2))
       .flatMap(v => {
-        val list = new Array[((Int, Int), Set[(Int, Int)])](v._2.size)
+        val list = new Array[(String, Set[String])](v._2.size)
         var i = 0
         for (value <- v._2) {
           list(i) = (value, v._2.filterNot(_ == value))
@@ -49,23 +44,11 @@ object Sindy {
         list
       })
       .reduceByKey((v1, v2) => v1.intersect(v2))
-      .flatMap(v => {
-        val list = new Array[((Int, Int), (Int, Int))](v._2.size)
-        var i = 0
-        for (value <- v._2) {
-          list(i) = (v._1, value)
-          i += 1
-        }
-        list
-      })
+      .filter(v => v._2.nonEmpty)
+      .sortBy(v => v._1)
       .collect()
-    for (bc <- fileIndices){
-      bc.destroy()
-    }
     for (dep <- result) {
-      println(s"${inputs(dep._1._1).substring(10).stripSuffix(".csv")} -> ${
-        inputs(dep._2._1).substring(10).stripSuffix(".csv")}: [${
-        names(dep._1._1)(dep._1._2)}] c [${names(dep._2._1)(dep._2._2)}]")
+      println(s"${dep._1} < ${dep._2.mkString("", ",", "")}")
     }
   }
 
